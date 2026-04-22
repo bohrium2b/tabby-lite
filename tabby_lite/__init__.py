35453f2172e6930ba352d620ed8f48cf67e92318
Dalton Ma <85321730+bohrium2b@users.noreply.github.com>
2026-04-22 01:08:24 +0000

    feat: Refactor Celery app initialization and add celery_app module

--- tabby_lite/__init__.py
+++ tabby_lite/__init__.py
@@ -1,4 +1,4 @@
-from .celery import app as celery_app
+from .celery_app import app as celery_app
 
 # Expose Celery app as a module-level variable for `celery -A tabby_lite` usage
 __all__ = ("celery_app",)
